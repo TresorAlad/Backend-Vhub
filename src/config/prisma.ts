2f571d1af02dfcc6971a49a1f7bfe325286ce5d8
It's Trésor <bernardalade92@gmail>
2026-04-29 13:31:22 +0000

    my modification

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -2,8 +2,18 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  console.error('CRITICAL: DATABASE_URL is not defined in environment variables.');
+}
+
 const pool = new pg.Pool({ connectionString });
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+});
+
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
